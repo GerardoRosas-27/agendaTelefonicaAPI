a4d8d0d622f6ffda0ce31b9e2bb664f11aa985e8
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-06-09 14:38:54 -0500

    ajustes conexion bd geroku

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'agendatelefonica'
+        host: '185.224.137.3',
+        user: 'u656398460_bruster1',
+        password: 'bruster',
+        database: 'u656398460_agenda'
     }
 };
--- src/keys.ts
+++ src/keys.ts
@@ -1,8 +1,8 @@
 export default {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'agendatelefonica'
+        host: '185.224.137.3',
+        user: 'u656398460_bruster1',
+        password: 'bruster',
+        database: 'u656398460_agenda'
     }
 }
