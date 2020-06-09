26aa9eaf32964a2bbb39c42933fe2ee12c4a6ff4
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-06-09 14:57:27 -0500

    eliminado typescript

--- src/keys.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    database: {
-        host: '185.224.137.3',
-        user: 'u656398460_bruster1',
-        password: 'bruster',
-        database: 'u656398460_agenda'
-    }
-}
